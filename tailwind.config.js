47824058c57e641d75ca0c4cd6b8535607284e12
Juan Alvarez <juanandres140299@gmail.com>
2021-03-02 22:05:37 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [
+    './*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'commissioner': ['Commissioner']
+      },
+      colors: {
+        cyan: {
+          DEFAULT: '#3CB4AC',
+          dark: '#147B74'
+        },
+      },
+      zIndex: {
+        '-10': '-10'
+      },
+      boxShadow: {
+        custom: 'inset 0px 50px 20px -30px rgba(0,0,0,0.75);',
+        container: '0 0px 60px -15px rgba(0, 0, 0, 0.5)'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
